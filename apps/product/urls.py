a1a00f63ffd45e11a5f914e9bd387a52c79362ab
jihuanli <jihuanli@iZ25ywls4e4Z.(none)>
2014-11-12 20:57:33 +0800

    add button in search views

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -4,5 +4,6 @@ from apps.product import views
 
 urlpatterns = patterns('',
   url(r'^(?P<product_id>\d+)/$', views.product_detail, name='question_detail'),
+  url(r'^(?P<product_id>\d+)/mark/(?P<mark>\d+)/$', views.question_list, name='question_list'),
 )
 
--- apps/product/views.py
+++ apps/product/views.py
@@ -2,24 +2,39 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from django.template import RequestContext, loader
+import json
 import requests
 from apps.api import api_list
 from apps.utils import string_utils
 
 def process_product_data(product_data):
   for question in product_data['questionList']:
-    question['relatedAnswer']['content'] = string_utils.truncate_text(question['relatedAnswer']['content']) 
-  for pic in product_data['product']['pics']:
-    pic['thumb_b'] = pic['thumb-b']
-    pic['thumb_s'] = pic['thumb-s']
-    pic['thumb_l'] = pic['thumb-l']
+    if question['relatedAnswer'] != None:
+      question['relatedAnswer']['content'] = string_utils.truncate_text(question['relatedAnswer']['content']) 
      
+def question_list(request, product_id, mark):
+  if request.is_ajax(): #仅接受ajax请求
+    try:
+      product_json = api_list.get_product_detail(product_id, 0, 20, mark)
+      if product_json['error'] == 0:
+        process_product_data(product_json)
+        template = loader.get_template("product/question_list.html")
+        context = RequestContext(request, {'product':product_json})
+        response_json = {'html':template.render(context), 'mark':product_json['mark']}
+        return HttpResponse(json.dumps(response_json), content_type="application/json") 
+    except Exception as e:
+      print e
+      return HttpResponse(e)
+    raise Http404
+  else:
+    raise Http404
   
 def product_detail(request, product_id):
   has_product_info = 1
   product_json = api_list.get_product_detail(product_id, has_product_info)
   try:
-    if product_json == None or product_json == "":
+    if product_json == None or product_json == "" or product_json['error'] != 0:
       return HttpResponse("product id invalid")
     process_product_data(product_json)
     return render(request, 'product/product.html', {'product':product_json})
