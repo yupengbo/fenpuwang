8c9c4acc931f562b44857f7f62a1aa98da7dd3bd
jihuanli <jihuanli@iZ25ywls4e4Z.(none)>
2014-11-12 13:55:31 +0800

    modify products in search template

--- apps/search/views.py
+++ apps/search/views.py
@@ -8,11 +8,21 @@ import requests
 from apps.api import api_list
 from apps.utils import string_utils
 
+def process_search_data(search_data):
+  for question in search_data['questionList']:
+    question['relatedAnswer']['content'] = string_utils.truncate_text(question['relatedAnswer']['content']) 
+  for product in search_data['productList']:
+    pics = product.get('pics')
+    if pics and len(pics) > 0:
+      product['thumb_s'] = pics[0]['thumb-s']
+
 def search(request, keyword):
   search_json = api_list.search(keyword)
   try:
     if search_json == None or search_json == "":
       return HttpResponse("search keyword invalid")
+    process_search_data(search_json)
+    print search_json
     return render(request, 'search/search.html', {'search':search_json})
   except Exception,e:
     print e
