df852c69c071e79062782593f351909a3d4d341e
zhangxl <zhangxl@dabanniu.com>
2014-11-13 16:54:07 +0800

    loaddata 增加点击 flush 处理

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse,Http404
 import requests
-from apps.api import api_list
+from apps.api import api_list, static_data
 from apps.utils import data_process_utils
 from apps.utils import string_utils
 import json
@@ -13,17 +13,18 @@ from django.core.urlresolvers import reverse
 def index(request):
     try:
         feed_obj = api_list.get_product_feeds()
+        nav_list = static_data.get_nav_data() 
         if feed_obj['error'] == 0 :
             process_feed_data(feed_obj)
             next_request_url = reverse('feed:feed_list', kwargs ={"categoryid" : 0, "mark" : feed_obj['mark']}) 
-            return render(request, 'feed/feeds.html', {'feeds' : feed_obj['feedList'], 'url' : next_request_url})
+            return render(request, 'feed/feeds.html', {'feeds' : feed_obj['feedList'],'nav_list' : nav_list , 'url' : next_request_url})
     except Exception,e:
         print e
         raise Http404
     raise Http404
 
 def feed_list(request, categoryid, mark):
-    if request.is_ajax(): #仅接受ajax请求
+#    if request.is_ajax(): #仅接受ajax请求
         try:
             feed_obj = api_list.get_product_feeds( categoryid, 20, mark)
             if feed_obj['error'] == 0:
@@ -36,8 +37,8 @@ def feed_list(request, categoryid, mark):
         except Exception as e:
             return HttpResponse(e)
         raise Http404
-    else:
-        raise Http404
+#    else:
+#        raise Http404
 
 def process_feed_data(feed_obj):
     """
--- static/js/loaddata.js
+++ static/js/loaddata.js
@@ -1,53 +1,87 @@
 
-jQuery.fn.loaddata = function( opts) {
-    opts = jQuery.extend({
-        url : "",
-        threshold : 100,
-        box : ".datalist",
-        load_mode : "scroll",//scroll  click
-        data_mode : "append",//append  flush
-        loading_mode : "appendBox",//appendBox  page
-        page_loading : "#data-loadding",
-        loading_img : '<img src="/static/images/icon_feed_loading.gif" />'
-    }, opts || {});
+jQuery.fn.loaddata = function( user_setting) {
     return this.each(function() {
+        
+        var opts = jQuery.extend({
+            /* config */
+            url : "",//显示指定url，如果未指定则从 url_attr中获取
+            threshold : 100,//滚动加载阈值
+            box : ".datalist",//容器
+            load_mode : "scroll_append",//scroll_append  click_flush  click_append
+            /* config */
+        }, user_setting || {});
+        
+        var sys_opts = {
+            page_loading : "data-loadding",//整页加载时容器ID
+            url_attr : "get_data_url",//容器 加载数据 来源
+            data_mode_attr :"data_mode",//设置容器加载数据后 的处理方式的attr
+            loading_img : '<img src="/static/images/icon_feed_loading.gif" />'
+        };
+        
+        var DATA_MODE_APPEND = "append";
+        var DATA_MODE_FLUSH = "flush";
+        
         function __init() {
-           
             if (!$.loaddata_loadding){
                 $.loaddata_loadding = false;
             }
-            if( opts.url =="" || opts.box == "" ){
+            if( opts.box == "" ){
                return;
             }
-            if(opts.load_mode == 'scroll'){
-               bind_scroll();
-            }else if(opts.load_mode == 'click'){
-               bind_click();
+            if (opts.url){
+                $currentObj.attr(sys_opts.url_attr, opts.url);
             }
+            if(!$currentObj.attr(sys_opts.url_attr)){
+                return;
+            }
+            
+            if(opts.load_mode == 'scroll_append'){
+                bind_scroll_append();
+            }else if(opts.load_mode == 'click_flush'){
+                bind_click(DATA_MODE_FLUSH)
+            }else if(opts.load_mode == 'click_append'){
+                bind_click(DATA_MODE_APPEND)
+            }
+            
         }
-        function bind_scroll(){
-            $(window).scroll(function () {
+        function bind_scroll_append(){
+            $(window).scroll(function(){
                 var srollPos = $(window).scrollTop()+$(window).height();    //滚动条距顶部距离(页面超出窗口的高度)
                 var flowLine = $(opts.box).offset().top+$(opts.box).outerHeight()-opts.threshold;
                 if(!$.loaddata_loadding){
                     if (flowLine < srollPos) {
+                        $(opts.box).attr(sys_opts.data_mode_attr, DATA_MODE_APPEND);
                         load_data();
                     } 
                 }
             });
         }
-        function bind_click(){
+        function bind_click(data_mode){
             $currentObj.click(function(){
-                load_data();
+                $(opts.box).attr(sys_opts.data_mode_attr, data_mode);
+                $(opts.box).attr(sys_opts.url_attr, $currentObj.attr(sys_opts.url_attr));
+                if(!$.loaddata_loadding){
+                    load_data();
+                }
+            });
+        }
+        
+        function bind_click_flush(){
+            $currentObj.click(function(){
+                $(opts.box).attr(sys_opts.data_mode_attr, DATA_MODE_FLUSH);
+                $(opts.box).attr(sys_opts.url_attr, $currentObj.attr(sys_opts.url_attr));
+                if(!$.loaddata_loadding){
+                    load_data();
+                }
             });
         }
         function load_data(){
            $.loaddata_loadding = true;
-           console.log("load---:"+$.loaddata_loadding);
            show_loading();
-           if (opts.url){
+           var request_url=$(opts.box).attr(sys_opts.url_attr);
+           if (request_url){
                $.get_data(
-                    opts.url, 
+                    request_url, 
                     "",
                     function(data) {
                         process_data(data);
@@ -61,16 +95,22 @@ jQuery.fn.loaddata = function( opts) {
         }
         var $loaddingItem ;
         function show_loading() {
-           if(opts.loading_mode == 'appendBox'){
+           var data_mode=$(opts.box).attr(sys_opts.data_mode_attr);
+           if(data_mode == DATA_MODE_APPEND){
               $loaddingItem = $(opts.box).children(":first").clone();
-              $loaddingItem.html(opts.loading_img);
+              $loaddingItem.html(sys_opts.loading_img);
               $loaddingItem.appendTo($(opts.box));
               $loaddingItem.css("text-align","center");
-           }else if(opts.loading_mode == 'page'){
-              if($(opts.page_loading).length == 0){
-                  var boxcss = 'background-color:#000;-moz-opacity:0.8; opacity:0.8;-khtml-opacity: 0.8;position:absolute;top:0px;left:0px;z-index:9999;display:hidden;';
-                  $loaddingItem = $('<div id="data-loadding" style="'+boxcss+'">'+opts.loading_img+'</div>');
+           }else if(data_mode == DATA_MODE_FLUSH){
+              var loaddingTag = "#"+sys_opts.page_loading;
+              $loaddingItem = $(loaddingTag);
+              var boxcss = 'background-color:#000;-moz-opacity:0.8; opacity:0.8;-khtml-opacity: 0.8;position:absolute;top:0px;left:0px;z-index:9999;display:hidden;';
+              if($loaddingItem.length == 0){
+                  $loaddingItem = $('<div id="'+data-loadding+'" style="display:none">'+sys_opts.loading_img+'</div>');
                   $("body").prepend($loaddingItem);
+              }
+              if(!$loaddingItem.attr("css_loaded")){
+                  $loaddingItem.attr("style",boxcss);
               }
               var ww = $(window).width();
               var wh = $(window).height();
@@ -78,32 +118,32 @@ jQuery.fn.loaddata = function( opts) {
               var dh = $(document).height();
               $loaddingItem.width(dw);
               $loaddingItem.height(dh);
-              $('#data-loadding img').css('position','fixed')
-              $('#data-loadding img').css('top', (wh / 2 - 24));
-              $('#data-loadding img').css('left', (ww / 2 - 24));
+              $(loaddingTag+' img').css('position','fixed')
+              $(loaddingTag+' img').css('top', (wh / 2 - 24));
+              $(loaddingTag+' img').css('left', (ww / 2 - 24));
               $loaddingItem.show();
            }
         }
         function close_loading() {
-           if(opts.loading_mode == 'appendBox'){
+           var data_mode=$(opts.box).attr(sys_opts.data_mode_attr);
+           if(data_mode == DATA_MODE_APPEND){
               $loaddingItem.remove();
-           }else if(opts.loading_mode == 'page'){
-              if($(opts.page_loading).length > 0){
-                  $loaddingItem.hide();
-              }
+           }else if(data_mode == DATA_MODE_FLUSH){
+              $loaddingItem.hide();
            }
            $.loaddata_loadding = false;
         }
         function process_data(data) {
-           if(opts.data_mode == 'append'){
+           var data_mode=$(opts.box).attr(sys_opts.data_mode_attr);
+           if(data_mode == DATA_MODE_APPEND){
               append_data(data)
-           }else if(opts.data_mode == 'flush'){
+           }else if(data_mode == DATA_MODE_FLUSH){
               flush_data(data)
            }
            if(data.url){
-               opts.url=data.url;
+               $(opts.box).attr(sys_opts.url_attr, data.url);
            }else{
-               opts.url="";
+               $(opts.box).attr(sys_opts.url_attr, "");
            }
         }
         function append_data(data) {
@@ -112,7 +152,7 @@ jQuery.fn.loaddata = function( opts) {
         function flush_data(data) {
            $(opts.box).html(data.html);
         }
-        $currentObj = $(this);
+        var $currentObj = $(this);
         __init();
     });
 }
