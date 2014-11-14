d1a38b3e813ce396159e55dce485b62bf938ccfd
jihuanli <jihuanli@iZ25ywls4e4Z.(none)>
2014-11-14 16:40:16 +0800

    add links in product & search

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from apps.product import views
 
 urlpatterns = patterns('',
-  url(r'^(?P<product_id>\d+)/$', views.product_detail, name='question_detail'),
+  url(r'^(?P<product_id>\d+)/$', views.product_detail, name='product_detail'),
   url(r'^(?P<product_id>\d+)/mark/(?P<mark>\d+)/$', views.question_list, name='question_list'),
 )
 
