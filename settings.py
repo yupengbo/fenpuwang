de19f8bae906a581457e6cf1dd07bdda99614f28
jihuanli <jihuanli@iZ25ywls4e4Z.(none)>
2014-11-17 17:40:17 +0800

    add deploy script for gunicorn

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
                   'apps.product',
                   'apps.products',
                   'apps.feed',
-                  'apps.search'
+                  'apps.search',
                   )
 
 MIDDLEWARE_CLASSES = (
