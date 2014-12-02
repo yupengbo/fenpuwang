8c437f0559db3c2b812537bd7b913f9eb71a19b4
zhangxl <zhangxl@dabanniu.com>
2014-12-02 16:35:19 +0800

    TOPIC详情 目录问题

--- apps/topic/urls.py
+++ apps/topic/urls.py
@@ -5,6 +5,6 @@ from apps.topic import views
 urlpatterns = patterns('',
   url(r'^$', views.topic_list , name='topic_index'),
   url(r'^mark/(?P<mark>\d+)$', views.topic_list, name='topic_list'),
-  url(r'^(?P<topic_id>\d+)$', views.topic_info, name='topic_info'),
+  url(r'^(?P<topic_id>\d+)/$', views.topic_info, name='topic_info'),
 )
 
