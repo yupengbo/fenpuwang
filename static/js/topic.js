186708ed16e4464af9e99c48282d1209bc9d21ea
zhangxl <zhangxl@dabanniu.com>
2015-01-09 15:54:44 +0800

    底部商品导航

--- static/js/topic.js
+++ static/js/topic.js
@@ -7,7 +7,7 @@ $(function(){
    $(".footer").html(link_html);
    $(".footer .topic_product_text").html("<div class='footer_product_tip'>本文提到的商品</div>" + 
       $(".footer .topic_product_text").html() +
-	  "<div class='fl footer_product_seller_info'><span class='seller_name'>卖家：美丽出品</span><span class='fenpu_info'>粉扑担保：直邮认证 无理由退货</span></div>" 
+	  "<div class='fr footer_product_seller_info'><span class='seller_name'>卖家：美丽出品</span><span class='fenpu_info'>粉扑担保：直邮认证 无理由退货</span></div>" 
    );
    $(".footer .topic_product_text .topic_product_price").addClass("fl");
    $(".footer .link_arrow .link_arrow_i").removeClass("link_arrow_i");
