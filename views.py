a9fba196a985a5d24e7c83ab6e48a012c5a791ea
zhangxl <zhangxl@dabanniu.com>
2015-01-23 15:42:04 +0800

    下载页面

--- views.py
+++ views.py
@@ -11,30 +11,4 @@ from django.core.urlresolvers import reverse
 # Create your views here.
 
 def download(request):
-    try:
-        feed_obj = api_list.get_product_feeds(request)
-        nav_list = static_data.get_nav_data() 
-        if feed_obj['error'] == 0 :
-            process_feed_data(feed_obj)
-            next_request_url = reverse('feed:feed_list', kwargs ={"categoryid" : 0, "mark": feed_obj['mark']}) 
-            meta = response_data_utils.pack_data(request, {'feeds' : feed_obj['feedList'],'nav_list' : nav_list , 'url' : next_request_url, "nav": "index"})
-            return render(request, 'feed/feeds.html', meta)
-        else:
-            return response_data_utils.error_response(request, None, __name__, feed_obj)
-    except Exception,e:
-        return response_data_utils.error_response(request, None, __name__, e)
-def process_feed_data(feed_obj):
-    """
-    process feed dictionary returned from api.
-    :param feed_obj:
-    :return:
-    """
-
-    for feed in feed_obj['feedList']:
-        feed['user']['profile'] = data_process_utils.get_user_profile_str(feed['user'])
-        if feed.get('answer'):
-            feed['answer']['content'] = string_utils.truncate_text(feed['answer']['content'])
-        if feed.get('question'):
-            feed['question']['content'] = string_utils.truncate_text(feed['question']['content'])
-        else:
-            feed['question'] = {"questionId":0}
+    return render(request, 'download.html')
