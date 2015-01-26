a97f0b659c9722a73696d4702ab4d86a3b20db5f
zhangxl <zhangxl@dabanniu.com>
2015-01-26 18:13:05 +0800

    样式加版本 以及设置精选为首页

--- urls.py
+++ urls.py
@@ -7,12 +7,12 @@ import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^(feed/)?', include('apps.feed.urls', namespace='feed')),
+    url(r'^feed/', include('apps.feed.urls', namespace='feed')),
     url(r'^question/', include('apps.question.urls', namespace='question')),
     url(r'^product/', include('apps.product.urls', namespace='product')),
     url(r'^products/', include('apps.products.urls', namespace='products')),
     url(r'^search/', include('apps.search.urls', namespace='search')),
-    url(r'^topic/', include('apps.topic.urls', namespace='topic')),
+    url(r'^(topic/)?', include('apps.topic.urls', namespace='topic')),
 	url(r'^ucenter/', include('apps.ucenter.urls', namespace='ucenter')),
     url(r'^download/$', views.download, name='download'),
 )
