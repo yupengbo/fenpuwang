ccc18949baa43593c7c310d9118348df89d8a2ac
root <root@iZ25ywls4e4Z.(none)>
2015-01-26 17:48:48 +0800

    修改url

--- apps/question/urls.py
+++ apps/question/urls.py
@@ -4,8 +4,8 @@ from apps.question import views
 
 urlpatterns = patterns('',
     url(r'^$',views.question_list,name='question_list'),
-    url(r'^mark/(?P<mark>\d+)/choice/$',views.question_list,name='question_list_ajax'),
-    url(r'^mark/(?P<mark>\d+)/new/$',views.new_question_list,name='question_list_new'),
+    url(r'^/choice/mark/(?P<mark>\d+)/$',views.question_list,name='question_list_choice'),
+    url(r'^/new/mark/(?P<mark>\d+)/$',views.new_question_list,name='question_list_new'),
     url(r'^(?P<question_id>\d+)/$', views.question_details, name='question_details'),
     url(r'^(?P<question_id>\d+)/mark/(?P<mark>\d+)/$', views.answer_list, name='answer_list'),
 )
