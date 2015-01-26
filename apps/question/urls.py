f6b58c66e7149f6884dc30fa42402a3dfa2a0f55
root <root@iZ25ywls4e4Z.(none)>
2015-01-26 12:31:40 +0800

    增加问答页url

--- apps/question/urls.py
+++ apps/question/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, url
 from apps.question import views
 
 urlpatterns = patterns('',
+    url(r'^$',views.question_list,name='question_list'),
+    url(r'^mark/(?P<mark>\d+)/choice/$',views.question_list,name='question_list_ajax'),
+    url(r'^mark/(?P<mark>\d+)/new/$',views.new_question_list,name='question_list_new'),
     url(r'^(?P<question_id>\d+)/$', views.question_details, name='question_details'),
     url(r'^(?P<question_id>\d+)/mark/(?P<mark>\d+)/$', views.answer_list, name='answer_list'),
 )
