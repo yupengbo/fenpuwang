7f44c65223925ffb94978d72cfc040354d6cdbdf
zhangxl <zhangxl@dabanniu.com>
2015-02-02 14:16:08 +0800

    我的收益 BUG

--- apps/api/api_list.py
+++ apps/api/api_list.py
@@ -98,6 +98,10 @@ def get_user_change_log(req, sessionKey, mark = 0):
     params = {'sessionKey': sessionKey,  'mark': mark}
     return get_result(req, 'listUserChangeLog.do', params)
 
+def get_user_change_info(req, sessionKey):
+    params = {'sessionKey': sessionKey}
+    return get_result(req, 'getUserChangeInfo.do', params)
+
 def get_activity_info(req, sessionKey, activity_id = 0):
     params = {'sessionKey': sessionKey, 'activityId': activity_id}
     return get_result(req, 'getActivityInfo.do', params)
