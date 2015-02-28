503d8312f0f6c9e737e760e8b12fa6ed71a9da3d
jihuanli <lijh@dabnniu.com>
2015-02-28 10:48:43 +0800

    修改产品官方页导航

--- apps/product/views.py
+++ apps/product/views.py
@@ -88,7 +88,7 @@ def product_detail(request, product_id):
     process_product_data(product_json)
     next_request_url = reverse('product:question_list', kwargs ={"product_id":product_id, "mark":product_json['mark']})
     meta = response_data_utils.pack_data(request, {'navTitle':'产品详情','product':product_json, 'url':next_request_url})
-    return weixin_auth_utils.fp_render(request,'product/product.html',meta, session)
+    return weixin_auth_utils.fp_render(request,'product/product.html', meta, session)
   except Exception,e:
     print e
     return response_data_utils.error_response(request, "找不到这个产品！",  __name__, e)
