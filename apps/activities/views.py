2d2ed9c4b4616f000862b5a979fa6d51196b1808
zhangxl <zhangxl@dabanniu.com>
2015-04-15 12:25:57 +0800

    校园活动 URL

--- apps/activities/views.py
+++ apps/activities/views.py
@@ -235,7 +235,7 @@ def result_new(request, activity_key=1):
    meta_data ['cant_valid'] = get_cant_valid(activity_key)
    return render(request, 'activities/water_result.html', meta_data)
 
-def get_university_address(activity_key):
+def get_university_name(activity_key):
    if not activity_key or activity_key == 1:
        return "燕京理工"
    elif activity_key == 'bdfa1841d81cbc3b7a3d8de93aedd357':
@@ -251,7 +251,7 @@ def get_cant_valid(activity_key):
    else:
        return "false"
 
-def get_university_name(activity_key):
+def get_university_address(activity_key):
    if not activity_key or activity_key == 1:
        return "医务室对面水吧"
    elif activity_key == 'bdfa1841d81cbc3b7a3d8de93aedd357':
