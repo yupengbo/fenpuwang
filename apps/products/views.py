87093ab41fdd1c6819bf58f589d7d9959fbd5b35
jihuanli <lijh@dabnniu.com>
2015-04-22 16:33:34 +0800

    秒杀跟踪修改

--- apps/products/views.py
+++ apps/products/views.py
@@ -137,6 +137,10 @@ def process_product_data(product_data):
             product['thumb_s'] = pics[0]['thumb-s']
 
 def seckill(request):
+    user_from = request.REQUEST.get('from')
+    if user_from == None or user_from == "":
+      user_from = "flashweb"
+
     user_agent = request.META.get('HTTP_USER_AGENT')
     is_fenpu = 0
     user_agent = user_agent.lower()
@@ -153,7 +157,7 @@ def seckill(request):
        is_flashing = 1
 #    seckill_today_list = {}
     server_time_stamp = seckill_result['timeStamp']
-    meta_data = {"is_flashing":is_flashing, "seckill_today_list":seckill_today_list,"seckill_tomorrow_list":seckill_tomorrow_list,'navTitle':'秒杀', "server_time_stamp": server_time_stamp, "is_fenpu":is_fenpu, "is_url":1, "nav_url":'/11286'}
+    meta_data = {"is_flashing":is_flashing, "seckill_today_list":seckill_today_list,"seckill_tomorrow_list":seckill_tomorrow_list,'navTitle':'秒杀', "server_time_stamp": server_time_stamp, "is_fenpu":is_fenpu, "is_url":1, "nav_url":'/11286', "user_from":user_from}
 #    meta_data = {"seckill_today_list":seckill_tomorrow_list,"seckill_tomorrow_list":seckill_tomorrow_list, "server_time_stamp": server_time_stamp}
     #if seckill_today_list:
     #    return render(request,"products/flashing.html",meta_data)
