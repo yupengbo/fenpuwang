35a106daacd288f8ab65728f51edf6b62adcc936
root <root@AY12041005283846c8064.(none)>
2015-04-27 18:32:09 +0800

    买买买 页面 lazy load

--- static/js/products.js
+++ static/js/products.js
@@ -18,19 +18,6 @@ $(function(){
 	 }
 	 return false;
   });
-  $(window).scroll(function () {
-      $(".brand_filter_box").hide();
-	  $(".brand_filter_btn").removeClass("brand_filter_btn_up");
-	  var srollPos = $(window).scrollTop();    //滚动条距顶部距离(页面超出窗口的高度)
-	  var flowLine = $('.filter_list_box').offset().top-44;
-	  if (flowLine < srollPos) {
-	      $(".filter_flow_box").css('position', 'fixed');
-	      $(".filter_flow_box").css('left',0);
-		  $(".filter_flow_box").css('top',0);
-	  } else {
-	      $(".filter_flow_box").css('position', 'static');
-	  }
-  });
   function show_filter_brand_box(){
     $brand_filter_btn = $(".brand_filter_btn");
     if($(".brand_filter_box").is(":hidden")){
